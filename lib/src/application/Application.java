1f977e977c804ffb8a51088dcbc2b4674f391977
JIANG Yizhi <zhishou221@hotmail.com>
2017-10-24 01:01:28 +0800

    - "Refine the output format"

--- lib/src/application/Application.java
+++ lib/src/application/Application.java
@@ -2,15 +2,15 @@ package application;
 
 public  class Application implements Printable{
     public Application(){
-
+        print("Constructing application and initializing resources....");
     }
 
     public void start(){
-
+        print("Story starts!");
     }
 
     public void end(){
-
+        print("Release resources. Story ends.");
     }
 
 

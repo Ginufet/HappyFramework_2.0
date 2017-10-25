7918603d4538aad193cd490d35dacc44655a62dc
absolutelynot <1056957459@qq.com>
2017-10-25 08:18:52 +0800

    AbstractHandler：责任链，抽象处理者 Director:抽象中介者 Scene在中介者模式中被视为抽象同事类

---
+++ lib/src/scene/Director.java
@@ -0,0 +1,8 @@
+package scene;
+
+import scene.template.Scene;
+
+public abstract class Director {
+    //场景切换函数，根据传入的信息和信息发送者进行判断后，对场景切换
+    public abstract void change(String message, Scene scene);
+}
--- lib/src/scene/template/Scene.java
+++ lib/src/scene/template/Scene.java
@@ -3,11 +3,23 @@ package scene.template;
 import application.Printable;
 import entity.Entity;
 import scene.Actionable;
+import scene.Director;
 
 import java.util.Collection;
 import java.util.HashMap;
 
 public abstract class Scene implements Printable, Actionable {
+
+    //中介者
+    protected Director mediator;
+    //可直接设置中介者
+    public Scene(Director mediator){
+        this.mediator = mediator;
+    }
+    //在抽象同事类中添加用于与中介者取得联系（即注册）的方法
+    public  void setMediator(Director mediator){
+        this.mediator = mediator;
+    }
 
     private String sceneName;
     private HashMap<String, Entity> children;

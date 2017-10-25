54367ef6051a43a5543117738cc54dc0c406af72
JIANG Yizhi <zhishou221@hotmail.com>
2017-10-26 02:39:52 +0800

    - Completed DP Adapter

--- lib/src/scene/template/SampleScene.java
+++ lib/src/scene/template/SampleScene.java
@@ -2,6 +2,7 @@ package scene.template;
 
 import allocator.Allocator;
 import allocator.Strategy;
+import behavior.Transform;
 import entity.animal.template.Dog;
 import entity.plant.template.AppleTree;
 import scene.Scene;
@@ -15,8 +16,13 @@ public class SampleScene extends Scene {
     public void action() {
         // Write your code here.
         // TODO: Create entity using factory method or abstract factory method
-        Dog dog = new Dog();
-        System.out.println((dog.getClass()));
+        Dog doggy = new Dog();
+        Transform<Dog, Dog> dogDogTransform = new Transform<>(doggy);
+        System.out.println(dogDogTransform.getSource().getClass());
+        dogDogTransform.transform();
+        System.out.println(dogDogTransform.getTarget());
+
+
 //        Allocator<Dog> dogAllocator = new Allocator<>();
 //        dogAllocator.setObtainArguments(Strategy.PURCHASE);
 //        Dog doggy = dogAllocator.obtain();
